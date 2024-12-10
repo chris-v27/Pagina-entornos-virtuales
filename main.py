6c7057a4215dd11850e401203e0663306af7fbec
Chris <vanegaschristian.0422@gmail.com>
2024-12-09 19:07:35 -0500

    agregar pagina de datos curiosos

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+facts_list = ["La mayoría de las personas que sufren adicción tecnológica experimentan un fuerte estrés cuando se encuentran fuera del área de cobertura de la red o no pueden utilizar sus dispositivos",
+              "Según un estudio realizado en 2018, más del 50% de las personas de entre 18 y 34 años se consideran dependientes de sus smartphones",
+              "El estudio de la dependencia tecnológica es una de las áreas más relevantes de la investigación científica moderna",
+              "Según un estudio de 2019, más del 60% de las personas responden a mensajes de trabajo en sus smartphones en los 15 minutos siguientes a salir del trabajo",
+              "Una forma de combatir la dependencia tecnológica es buscar actividades que aporten placer y mejoren el estado de ánimo",
+              "Las redes sociales tienen aspectos positivos y negativos, y debemos ser conscientes de ambos cuando utilicemos estas plataformas",
+              ]
+
 @app.route("/")
 def hello_world():
-    return '<a href="/miRuta">Siguiente ruta</a>'
-
-@app.route("/miRuta")
+    return '<h1>Esta página da datos interesantes sobre las dependencias tecnológicas</h1><a href="/random_fact">¡Ver un dato aleatorio!</a>'
+    
+@app.route("/random_fact")
 def rutaMia():
-    return 'Mi propia ruta'
+    return f'<p>{random.choice(facts_list)}</p>''<a href="/">Regresar a la pagina principal</a>'
 
 app.run(debug=True)
